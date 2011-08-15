1174ed292ec3179f16b866a8f945bc826b77efe9
John McAliley <john.mcaliley@gmail.com>
2011-08-14 22:19:08 -0400

    thin config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "calagator_techclt"
-set :repository,  "git@github.com:charlotte-ruby/calagator.git"
+set :repository,  "git@github.com:charlotte-ruby/calagator_techclt.git"
 set :scm, :git
 set :use_sudo,    false
 set :deploy_to,   "/rails_apps/#{application}"
@@ -10,21 +10,29 @@ ssh_options[:forward_agent] = true
 
 server "174.143.150.37", :app, :web, :db, :primary => true
 
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+set :rvm_bin_path, "/home/app_user/.rvm/bin/"
+set :rvm_ruby_string, '1.8.7'
+set :rvm_type, :user
+
 namespace :deploy do
   task :start, :roles => :app do
-    #run "touch #{current_release}/tmp/restart.txt"
-    run "thin start -C /rails_apps/#{application}/current/config/thin.yml" #Thin
+    run "~/.rvm/gems/ruby-1.8.7-p334/bin/thin start -C /rails_apps/#{application}/current/config/thin.yml" #Thin
   end
 
   task :stop, :roles => :app do
     # Do nothing.
-    run "thin stop -C /rails_apps/#{application}/current/config/thin.yml" #Thin
+    run "~/.rvm/gems/ruby-1.8.7-p334/bin/thin stop -C /rails_apps/#{application}/current/config/thin.yml" #Thin
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    #run "touch #{current_release}/tmp/restart.txt"
-    run "thin restart --onebyone -C /rails_apps/#{application}/current/config/thin.yml" #Thin
+    run "~/.rvm/gems/ruby-1.8.7-p334/bin/thin restart --onebyone -C /rails_apps/#{application}/current/config/thin.yml" #Thin
   end
   
   desc "Stop Nginx"
