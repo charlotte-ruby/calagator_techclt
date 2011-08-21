3260b131a144d0c0ce910d94827e470e66723a23
John McAliley <john.mcaliley@gmail.com>
2011-08-21 16:44:43 -0400

    change to mysql and mongrel_cluster.  new deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,30 +13,24 @@ server "174.143.150.37", :app, :web, :db, :primary => true
 # Add RVM's lib directory to the load path.
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 
-# Load RVM's capistrano plugin.    
+# Load RVM's capistrano plugin.
 require "rvm/capistrano"
 
-set :rvm_bin_path, "/home/app_user/.rvm/bin/"
-set :rvm_ruby_string, '1.8.7'
+set :rvm_bin_path, "$HOME/.rvm/bin"
+set :rvm_ruby_string, '1.8.7@calagator'
 set :rvm_type, :user
 
 namespace :deploy do
   task :start, :roles => :app do
-    run "~/.rvm/gems/ruby-1.8.7-p334/bin/thin start -C /rails_apps/#{application}/current/config/thin.yml" #Thin
+    run "cd /rails_apps/#{application}/current;mongrel_rails cluster::start"
   end
 
   task :stop, :roles => :app do
-    # Do nothing.
-    run "~/.rvm/gems/ruby-1.8.7-p334/bin/thin stop -C /rails_apps/#{application}/current/config/thin.yml" #Thin
+    run "cd /rails_apps/#{application}/current;mongrel_rails cluster::stop"
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "~/.rvm/gems/ruby-1.8.7-p334/bin/thin restart --onebyone -C /rails_apps/#{application}/current/config/thin.yml" #Thin
-  end
-  
-  desc "Stop Nginx"
-  task :stop_nginx, :roles => :app do
-    run ""
+    run "cd /rails_apps/#{application}/current;mongrel_rails cluster::restart"
   end
 end
